33521eba41c576d5fcd737b699074c20f13bd44c
Alexander A. Klimov <grandmaster@al2klimov.de>
2021-06-03 14:32:41 +0200

    Introduce GetStack()

--- errors.go
+++ errors.go
@@ -1,6 +1,10 @@
 package fuel
 
-import "github.com/pkg/errors"
+import (
+	"github.com/pkg/errors"
+	"runtime"
+	"unsafe"
+)
 
 type ErrorWithStack interface {
 	error
@@ -9,4 +13,32 @@ type ErrorWithStack interface {
 
 type StackTracer interface {
 	StackTrace() errors.StackTrace
+}
+
+// errors.StackTrace is []uintptr
+var (
+	_ []errors.Frame = errors.StackTrace(nil)
+	_                = (*uintptr)((*errors.Frame)(nil))
+)
+
+// GetStack returns a complete errors.StackTrace of the calling goroutine
+// without GetStack itself and $skip additional frames at the top.
+func GetStack(skip int) errors.StackTrace {
+	frames := 32
+	for {
+		stack := make(errors.StackTrace, frames)
+
+		actual := runtime.Callers(
+			1+ // runtime.Callers
+				1+ // GetStack
+				skip,
+			*(*[]uintptr)(unsafe.Pointer(&stack)), // errors.StackTrace is []uintptr
+		)
+
+		if actual < frames {
+			return stack[:actual]
+		}
+
+		frames *= 2
+	}
 }
--- errors_test.go
+++ errors_test.go
@@ -1,12 +1,55 @@
 package fuel
 
 import (
+	"fmt"
 	"github.com/pkg/errors"
+	"strings"
 	"testing"
 )
 
 func TestErrorWithStack(t *testing.T) {
 	if _, ok := errors.New("").(ErrorWithStack); !ok {
 		t.Error("ErrorWithStack doesn't cover github.com/pkg/errors errors")
+	}
+}
+
+func TestGetStack(t *testing.T) {
+	if stack := GetStack(0); len(stack) < 1 {
+		t.Error("GetStack(0): stack empty")
+	} else {
+		for _, ptr := range stack {
+			if ptr == 0 {
+				t.Error("GetStack(0): has a 0x0 frame")
+			}
+		}
+
+		actual := fmt.Sprintf("%n", stack[0])
+		if !strings.Contains(actual, "TestGetStack") {
+			t.Errorf("GetStack(0): got %s on top of the stack, expected TestGetStack", actual)
+		}
+	}
+
+	if stack := GetStack(1); len(stack) > 0 {
+		actual := fmt.Sprintf("%n", stack[0])
+		if strings.Contains(actual, "TestGetStack") {
+			t.Error("GetStack(1): got TestGetStack on top of the stack")
+		}
+	}
+
+	const expected = 64
+	var actual errors.StackTrace
+
+	recurse(expected, func() { actual = GetStack(0) })
+
+	if len(actual) < expected {
+		t.Errorf("GetStack(0): got %d frames, expected >=%d", len(actual), expected)
+	}
+}
+
+func recurse(steps uint8, finally func()) {
+	if steps > 0 {
+		recurse(steps-1, finally)
+	} else {
+		finally()
 	}
 }
