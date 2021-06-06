8bce622b8376f11af6a1bb0ac98811d78a7dc6c0
Alexander A. Klimov <grandmaster@al2klimov.de>
2021-06-06 14:45:27 +0200

    Introduce AdvancedError

--- errors.go
+++ errors.go
@@ -1,10 +1,18 @@
 package fuel
 
 import (
+	"bytes"
+	"encoding"
+	"encoding/json"
+	"fmt"
 	"github.com/pkg/errors"
 	"runtime"
 	"unsafe"
 )
+
+type Causer interface {
+	Cause() error
+}
 
 type ErrorWithStack interface {
 	error
@@ -15,11 +23,9 @@ type StackTracer interface {
 	StackTrace() errors.StackTrace
 }
 
-// errors.StackTrace is []uintptr
-var (
-	_ []errors.Frame = errors.StackTrace(nil)
-	_                = (*uintptr)((*errors.Frame)(nil))
-)
+type Unwrapper interface {
+	Unwrap() error
+}
 
 // GetStack returns a complete errors.StackTrace of the calling goroutine
 // without GetStack itself and $skip additional frames at the top.
@@ -32,7 +38,7 @@ func GetStack(skip int) errors.StackTrace {
 			1+ // runtime.Callers
 				1+ // GetStack
 				skip,
-			*(*[]uintptr)(unsafe.Pointer(&stack)), // errors.StackTrace is []uintptr
+			stackAsRaw(stack),
 		)
 
 		if actual < frames {
@@ -41,4 +47,106 @@ func GetStack(skip int) errors.StackTrace {
 
 		frames *= 2
 	}
+}
+
+// AdvancedError is a feature-rich error wrapper.
+type AdvancedError struct {
+	Err   error
+	Stack errors.StackTrace
+}
+
+var _ Causer = AdvancedError{}
+
+func (ae AdvancedError) Cause() error {
+	return ae.Err
+}
+
+var _ error = AdvancedError{}
+
+func (ae AdvancedError) Error() string {
+	return ae.Err.Error()
+}
+
+var _ fmt.Formatter = AdvancedError{}
+
+// Format appends the stack on %+v.
+func (ae AdvancedError) Format(fs fmt.State, verb rune) {
+	FormatNonFormatter(fs, verb, ae.Err)
+
+	if verb == 'v' && fs.Flag('+') {
+		ae.Stack.Format(fs, verb)
+	}
+}
+
+var _ json.Marshaler = AdvancedError{}
+
+func (ae AdvancedError) MarshalJSON() ([]byte, error) {
+	type frame = struct {
+		File     string `json:"file,omitempty"`
+		Line     int    `json:"line,omitempty"`
+		Function string `json:"function,omitempty"`
+	}
+
+	var err interface{}
+	switch ae.Err.(type) {
+	case json.Marshaler, encoding.TextMarshaler:
+		err = ae.Err
+	default:
+		err = ae.Err.Error()
+	}
+
+	var stack []frame
+	frames := runtime.CallersFrames(stackAsRaw(ae.Stack))
+
+	for {
+		fr, ok := frames.Next()
+		if !ok {
+			break
+		}
+
+		stack = append(stack, frame{fr.File, fr.Line, fr.Function})
+	}
+
+	return json.Marshal(struct {
+		Error interface{} `json:"error"`
+		Stack []frame     `json:"stack,omitempty"`
+	}{err, stack})
+}
+
+var _ StackTracer = AdvancedError{}
+
+func (ae AdvancedError) StackTrace() errors.StackTrace {
+	return ae.Stack
+}
+
+var _ fmt.Stringer = AdvancedError{}
+
+func (ae AdvancedError) String() string {
+	s, _ := ae.MarshalText()
+	return string(s)
+}
+
+var _ encoding.TextMarshaler = AdvancedError{}
+
+func (ae AdvancedError) MarshalText() (text []byte, err error) {
+	buf := &bytes.Buffer{}
+	ae.Format(&Formatable{Output: buf, Flags: map[int]struct{}{'+': {}}}, 'v')
+
+	return buf.Bytes(), nil
+}
+
+var _ Unwrapper = AdvancedError{}
+
+func (ae AdvancedError) Unwrap() error {
+	return ae.Err
+}
+
+// errors.StackTrace is []uintptr
+var (
+	_ []errors.Frame = errors.StackTrace(nil)
+	_                = (*uintptr)((*errors.Frame)(nil))
+)
+
+func stackAsRaw(stack errors.StackTrace) []uintptr {
+	return *(*[]uintptr)(unsafe.Pointer(&stack)) // errors.StackTrace is []uintptr
 }
--- errors_test.go
+++ errors_test.go
@@ -1,8 +1,12 @@
 package fuel
 
 import (
+	"bytes"
+	"encoding"
+	"encoding/json"
 	"fmt"
 	"github.com/pkg/errors"
+	"io"
 	"strings"
 	"testing"
 )
@@ -46,10 +50,245 @@ func TestGetStack(t *testing.T) {
 	}
 }
 
+func TestAdvancedError_Format(t *testing.T) {
+	const dummy = "42"
+
+	assertAdvancedError_Format(
+		t, testFormatterError{testFormatter{dummy}, pseudoError{}}, &Formatable{Wid: 2, HasWid: true}, 'v',
+		func(actual []byte) string {
+			const expected = "self=42, state=2, verb=v"
+			if string(actual) == expected {
+				return ""
+			} else {
+				return fmt.Sprintf(" expected %#v", expected)
+			}
+		},
+	)
+
+	assertAdvancedError_Format(
+		t, testFormatterError{testFormatter{dummy}, pseudoError{}},
+		&Formatable{Wid: 2, HasWid: true, Flags: map[int]struct{}{'+': {}}}, 's',
+		func(actual []byte) string {
+			const expected = "self=42, state=+2, verb=s"
+			if string(actual) == expected {
+				return ""
+			} else {
+				return fmt.Sprintf(" expected %#v", expected)
+			}
+		},
+	)
+
+	assertAdvancedError_Format(
+		t, io.EOF, &Formatable{Flags: map[int]struct{}{'+': {}}}, 'v',
+		func(actual []byte) string {
+			if lines := bytes.Count(actual, []byte{'\n'}); lines < 64 {
+				return ", too few lines"
+			} else {
+				return ""
+			}
+		},
+	)
+}
+
+func assertAdvancedError_Format(t *testing.T, err error, fs *Formatable, verb rune, validator func([]byte) string) {
+	t.Helper()
+
+	buf := &bytes.Buffer{}
+	fs.Output = buf
+
+	var stack errors.StackTrace
+	recurse(64, func() {
+		stack = GetStack(0)
+	})
+
+	AdvancedError{err, stack}.Format(fs, verb)
+
+	if reason := validator(buf.Bytes()); reason != "" {
+		t.Errorf("AdvancedError{%#v, %#v}.Format(%#v, '%c'): got %#v%s", err, stack, fs, verb, buf.String(), reason)
+	}
+}
+
+func TestAdvancedError_MarshalJSON(t *testing.T) {
+	err1 := io.EOF
+	stack := GetStack(0)
+
+	if jsn, err := (AdvancedError{err1, stack}.MarshalJSON()); err == nil {
+		var tree interface{}
+		if err := json.Unmarshal(jsn, &tree); err == nil {
+			if root, ok := tree.(map[string]interface{}); ok {
+				if errorBranch, ok := root["error"]; ok {
+					if errorString, ok := errorBranch.(string); ok {
+						if errorString != err1.Error() {
+							t.Errorf(
+								"AdvancedError{%#v, %#v}.MarshalJSON(): got .error %#v, expected %#v",
+								err1, stack, errorString, err1.Error(),
+							)
+						}
+					} else {
+						t.Errorf(
+							"AdvancedError{%#v, %#v}.MarshalJSON(): got %#v, .error is not a string",
+							err1, stack, string(jsn),
+						)
+					}
+				} else {
+					t.Errorf("AdvancedError{%#v, %#v}.MarshalJSON(): got %#v, .error missing", err1, stack, string(jsn))
+				}
+
+				if stackBranch, ok := root["stack"]; ok {
+					if stackArray, ok := stackBranch.([]interface{}); ok {
+						if len(stackArray) < 1 {
+							t.Errorf(
+								"AdvancedError{%#v, %#v}.MarshalJSON(): got %#v, .stack is empty",
+								err1, stack, string(jsn),
+							)
+						} else if frameObject, ok := stackArray[0].(map[string]interface{}); ok {
+							if fileBranch, ok := frameObject["file"]; ok {
+								if _, ok := fileBranch.(string); !ok {
+									t.Errorf(
+										"AdvancedError{%#v, %#v}.MarshalJSON(): got %#v,"+
+											" .stack[0].file is not a string",
+										err1, stack, string(jsn),
+									)
+								}
+							} else {
+								t.Errorf(
+									"AdvancedError{%#v, %#v}.MarshalJSON(): got %#v, .stack[0].file missing",
+									err1, stack, string(jsn),
+								)
+							}
+
+							if lineBranch, ok := frameObject["line"]; ok {
+								if _, ok := lineBranch.(float64); !ok {
+									t.Errorf(
+										"AdvancedError{%#v, %#v}.MarshalJSON(): got %#v,"+
+											" .stack[0].line is not a number",
+										err1, stack, string(jsn),
+									)
+								}
+							} else {
+								t.Errorf(
+									"AdvancedError{%#v, %#v}.MarshalJSON(): got %#v, .stack[0].line missing",
+									err1, stack, string(jsn),
+								)
+							}
+
+							if functionBranch, ok := frameObject["function"]; ok {
+								if _, ok := functionBranch.(string); !ok {
+									t.Errorf(
+										"AdvancedError{%#v, %#v}.MarshalJSON(): got %#v,"+
+											" .stack[0].function is not a string",
+										err1, stack, string(jsn),
+									)
+								}
+							} else {
+								t.Errorf(
+									"AdvancedError{%#v, %#v}.MarshalJSON(): got %#v, .stack[0].function missing",
+									err1, stack, string(jsn),
+								)
+							}
+						} else {
+							t.Errorf(
+								"AdvancedError{%#v, %#v}.MarshalJSON(): got %#v, .stack[0] is not an object",
+								err1, stack, string(jsn),
+							)
+						}
+					} else {
+						t.Errorf(
+							"AdvancedError{%#v, %#v}.MarshalJSON(): got %#v, .stack is not an array",
+							err1, stack, string(jsn),
+						)
+					}
+				} else {
+					t.Errorf("AdvancedError{%#v, %#v}.MarshalJSON(): got %#v, .stack missing", err1, stack, string(jsn))
+				}
+			} else {
+				t.Errorf(
+					"AdvancedError{%#v, %#v}.MarshalJSON(): got %#v, root is not an object",
+					err1, stack, string(jsn),
+				)
+			}
+		} else {
+			t.Errorf(
+				"AdvancedError{%#v, %#v}.MarshalJSON(): got bad JSON %#v: %s",
+				err1, stack, string(jsn), err.Error(),
+			)
+		}
+	} else {
+		t.Errorf("AdvancedError{%#v, %#v}.MarshalJSON(): got %#v, expected nil", err1, stack, err)
+	}
+
+	err2 := testJsonError{jsn: []byte("1e42")}
+	if jsn, err := (AdvancedError{err2, nil}.MarshalJSON()); err == nil {
+		const expected = `{"error":1e42}`
+		if bytes.Compare(jsn, []byte(expected)) != 0 {
+			t.Errorf("AdvancedError{%#v, nil}.MarshalJSON(): got %#v, expected %#v", err2, string(jsn), expected)
+		}
+	} else {
+		t.Errorf("AdvancedError{%#v, nil}.MarshalJSON(): got %#v, expected nil", err2, err)
+	}
+
+	err3 := testTextError{text: []byte("42")}
+	if jsn, err := (AdvancedError{err3, nil}.MarshalJSON()); err == nil {
+		const expected = `{"error":"42"}`
+		if bytes.Compare(jsn, []byte(expected)) != 0 {
+			t.Errorf("AdvancedError{%#v, nil}.MarshalJSON(): got %#v, expected %#v", err3, string(jsn), expected)
+		}
+	} else {
+		t.Errorf("AdvancedError{%#v, nil}.MarshalJSON(): got %#v, expected nil", err3, err)
+	}
+}
+
 func recurse(steps uint8, finally func()) {
 	if steps > 0 {
 		recurse(steps-1, finally)
 	} else {
 		finally()
 	}
+}
+
+type pseudoError struct {
+}
+
+var _ error = pseudoError{}
+
+func (pseudoError) Error() string {
+	panic("don't call me")
+}
+
+type testFormatterError struct {
+	testFormatter
+	pseudoError
+}
+
+var (
+	_ fmt.Formatter = testFormatterError{}
+	_ error         = testFormatterError{}
+)
+
+type testJsonError struct {
+	pseudoError
+
+	jsn []byte
+}
+
+var _ error = testJsonError{}
+
+var _ json.Marshaler = testJsonError{}
+
+func (tje testJsonError) MarshalJSON() ([]byte, error) {
+	return tje.jsn, nil
+}
+
+type testTextError struct {
+	pseudoError
+
+	text []byte
+}
+
+var _ error = testTextError{}
+
+var _ encoding.TextMarshaler = testTextError{}
+
+func (tte testTextError) MarshalText() ([]byte, error) {
+	return tte.text, nil
 }
